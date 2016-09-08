6b0fa88fc3ef08915d7fe66519cb4cd58dc32f44
Elliott Sobek <elliottsobek@gmail.com>
2016-09-07 22:17:01 -0600

    Improve memory crash exception

--- pst.h
+++ pst.h
@@ -1,21 +1,21 @@
 /*
- * Password Time Calculator
- * Copyright (C) 2016  Author: Elliott Sobek
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+* Password Time Calculator
+* Copyright (C) 2016  Author: Elliott Sobek
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 #include <stdbool.h>
 
- /* Guard */
+/* Guard */
 #ifndef PST_H
 #define PST_H
 
@@ -25,7 +25,7 @@
 #define MONTHS 2678400 // 31 days
 #define YEARS 31536000 // 365 days
 
- /* Globals */
+/* Globals */
 unsigned int passwordAttackRate;
 unsigned short int passwordLength;
 unsigned short int numberOfCharacters;
@@ -58,11 +58,12 @@ void setTimeUnit(void);
 void setPasswordLength(void);
 void setPasswordAttackRate(void);
 void setNumberOfCharacters(void);
+void waitFor(unsigned int);
 void checkMalloc(char *);
 char * getNumberOfCharactersMenu(void);
 char * getTimeMenu(void);
 bool isValidBounds(int, short int, int);
-short int getAMenuInput( char *);
+short int getAMenuInput(char *);
 short int getPasswordLengthInput(void);
 double calculateNumberOfPasswords(void);
 #endif // !PST_H
